9db56ce06eb71f5fa5a07d1b4be2b11e87812627
DrGrodus <ebermd00@gmail.com>
2021-11-22 20:16:40 -0500

    Se agrego el proyecto

---
+++ ListaDeOrden/src/listadeorden/ListaDeOrden.java
@@ -0,0 +1,27 @@
+package listadeorden;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author eber
+ */
+public class ListaDeOrden {
+    public static void main(String[] args) {
+        List lista = new ArrayList<String>();
+        
+        lista.add("Chocolate");
+        lista.add("12345");
+        lista.add("Cadena de caracteres");
+        lista.add("numero en letras");
+        lista.add("ABCDE abcde");
+        lista.add("cHeese");
+        lista.add("D'oh");
+        lista.add("Bort");
+        lista.add("Carambanos de hielo");
+        lista.add("Repampanos");
+        lista.add("a");
+    }
+    
+}
